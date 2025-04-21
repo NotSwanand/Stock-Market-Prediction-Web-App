c66360a27d1896027a47abdc3374a7c2914fdc7c
NotSwanand <145210290+NotSwanand@users.noreply.github.com>
2025-04-21 23:50:35 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = "your_key"
+CONSUMER_SECRET = "your_secret"
+ACCESS_TOKEN = "your_token"
+ACCESS_TOKEN_SECRET = "your_token_secret"
+BEARER_TOKEN = "your_bearer_token"
+NUM_OF_TWEETS = 100  # or any number
